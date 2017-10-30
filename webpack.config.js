6e442e028785733495d6dfdcd8d60a71d21c1496
Nate <palmern22@gmail.com>
2017-10-30 06:31:18 -0400

    Initial react Setup

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
                 test: /\.jsx?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
             }
         ]
     },
