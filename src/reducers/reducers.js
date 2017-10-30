e479aa6f239addef3272d04619274e68ba91e1db
Nate <palmern22@gmail.com>
2017-10-30 06:47:24 -0400

    Adding redux and initial reducer

---
+++ src/reducers/reducers.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+    name: 'Nate'
+}
+
+function pitchCounter ( state, action ) {
+    if ( typeof state === 'undefined' ){
+        return initialState;
+    }
+
+    // TODO: Implement
+    
+    return state;
+}
