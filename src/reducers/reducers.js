242ad2e1045adf5164a6d3071efdacff558dd7aa
Nate <palmern22@gmail.com>
2017-10-31 21:00:28 -0400

    Setup Redux
    Add a store, hook it up.

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,14 +1,18 @@
+import { EDIT_NAME } from '../actions/actions';
 
 const initialState = {
     name: 'Nate'
 }
 
-function pitchCounter ( state, action ) {
-    if ( typeof state === 'undefined' ){
-        return initialState;
+function pitchCounter ( state = initialState, action ) {
+    switch( action.type ) {
+        case EDIT_NAME: 
+            return Object.assign({}, state, {
+                name: action.text
+            } );
+        default:
+            return state;
     }
-
-    // TODO: Implement
-    
-    return state;
 }
+
+export default pitchCounter;
--- src/stores/state-store.js
+++ src/stores/state-store.js
@@ -1,1 +1,4 @@
 import { createStore } from 'redux';
+import { pitchCounter } from '../reducers/reducers.js';
+
+let store = createStore( pitchCounter );
